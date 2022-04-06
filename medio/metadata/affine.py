103656232d68ba7cebd640eef942e6aea3e79e17
Jonathan Daniel <jonathand655@gmail.com>
2022-04-07 00:01:34 +0300

    Add typing to `affine`

--- medio/metadata/affine.py
+++ medio/metadata/affine.py
@@ -1,7 +1,31 @@
+from typing import Optional, Tuple, Union, Any, TYPE_CHECKING
+
 import numpy as np
+from numpy.typing import ArrayLike, NDArray
+
+if TYPE_CHECKING:
+    import sys
+
+    if sys.version_info.major < 3:
+        raise ValueError("Python 2 is not supported by `medio`.")
+
+    if sys.version_info.minor < 10:
+        # See:
+        # https://mypy.readthedocs.io/en/stable/common_issues.html#variables-vs-type-aliases
+        from typing_extensions import TypeAlias
+    else:
+        from typing import TypeAlias  # type:ignore[attr-defined,no-redef]
+
+    NDArrayFloat: TypeAlias = NDArray[np.float64]
+else:
+    NDArrayFloat = Any
 
 
-class Affine(np.ndarray):
+_IndexType = Union[int, slice, Any]  # Any is for Ellipsis
+_ItemType2D = Tuple[_IndexType, _IndexType]
+
+
+class Affine(NDArray[np.float64]):
     """
     Class for general (d+1)x(d+1) affine matrices, and in particular d=3 (3d space)
     Usage examples:
@@ -17,7 +41,14 @@ class Affine(np.ndarray):
     _origin_key = (slice(-1), -1)
     _m_key = (slice(-1), slice(-1))
 
-    def __new__(cls, affine=None, *, direction=None, spacing=None, origin=None):
+    def __new__(
+        cls,
+        affine: Optional[ArrayLike] = None,
+        *,
+        direction: Optional[ArrayLike] = None,
+        spacing: Optional[ArrayLike] = None,
+        origin: Optional[ArrayLike] = None,
+    ) -> "Affine":
         """
         Construct a numpy array of class Affine. Initialize Affine in one of the following ways:
         1. (d+1)x(d+1) matrix as affine (d is the dimension of the space)
@@ -34,7 +65,14 @@ class Affine(np.ndarray):
         obj = np.asarray(affine).view(cls)  # return array view of type Affine
         return obj
 
-    def __init__(self, affine=None, *, direction=None, spacing=None, origin=None):
+    def __init__(
+        self,
+        affine: Optional[ArrayLike] = None,
+        *,
+        direction: Optional[ArrayLike] = None,
+        spacing: Optional[ArrayLike] = None,
+        origin: Optional[ArrayLike] = None,
+    ):
         self.dim = self.shape[0] - 1
         if affine is None:
             self._spacing = np.asarray(spacing)
@@ -44,61 +82,63 @@ class Affine(np.ndarray):
             self._spacing = self.affine2spacing(self)
             self._direction = self.affine2direction(self, self.spacing)
 
-    def index2coord(self, index_vector):
+    def index2coord(self, index_vector: ArrayLike) -> np.ndarray:
         """Return y according to y = M*x + b"""
         return self._m_matrix @ index_vector + self.origin
 
-    def __matmul__(self, other):
+    def __matmul__(self, other: ArrayLike) -> np.ndarray:
         return super().__matmul__(other).view(np.ndarray)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: _ItemType2D) -> np.ndarray:
         return super().__getitem__(item).view(np.ndarray)
 
-    def clone(self):
+    def clone(self) -> "Affine":
         return Affine(self.copy())
 
     # Affine properties in addition to the numpy array
     @property
-    def origin(self):
+    def origin(self) -> NDArrayFloat:
         return self[self._origin_key]
 
     @origin.setter
-    def origin(self, value):
+    def origin(self, value: ArrayLike) -> None:
         self[self._origin_key] = value
 
     @property
-    def spacing(self):
+    def spacing(self) -> np.ndarray:
         return self._spacing
 
     @spacing.setter
-    def spacing(self, value):
+    def spacing(self, value: ArrayLike) -> None:
         value = np.asarray(value)
         self._m_matrix = self._m_matrix @ np.diag(value / self._spacing)
         # the spacing must be positive (or at least nonnegative)
         self._spacing = np.abs(value)
 
     @property
-    def direction(self):
+    def direction(self) -> np.ndarray:
         return self._direction
 
     @direction.setter
-    def direction(self, value):
+    def direction(self, value: ArrayLike) -> None:
         value = np.asarray(value)
         self._m_matrix = value @ np.diag(self.spacing)
         self._direction = value
 
     # Internal property - m matrix
     @property
-    def _m_matrix(self):
+    def _m_matrix(self) -> NDArrayFloat:
         return self[self._m_key]
 
     @_m_matrix.setter
-    def _m_matrix(self, value):
+    def _m_matrix(self, value: NDArrayFloat) -> None:
         self[self._m_key] = value
 
     # Static methods for affine construction and components
     @staticmethod
-    def construct_affine(direction, spacing, origin):
+    def construct_affine(
+        direction: ArrayLike, spacing: ArrayLike, origin: ArrayLike
+    ) -> np.ndarray:
         direction = np.asarray(direction)
         dim = direction.shape[0]
         affine = np.eye(dim + 1)
@@ -107,22 +147,26 @@ class Affine(np.ndarray):
         return affine
 
     @staticmethod
-    def affine2origin(affine):
+    def affine2origin(affine: NDArrayFloat) -> NDArrayFloat:
         return affine[Affine._origin_key]
 
     @staticmethod
-    def affine2spacing(affine):
+    def affine2spacing(affine: NDArrayFloat) -> NDArrayFloat:
         dim = affine.shape[0] - 1
         return np.linalg.norm(affine[Affine._m_key] @ np.eye(dim), axis=0)
 
     @staticmethod
-    def affine2direction(affine, spacing=None):
+    def affine2direction(
+        affine: NDArrayFloat, spacing: Optional[np.ndarray] = None
+    ) -> NDArrayFloat:
         if spacing is None:
             spacing = Affine.affine2spacing(affine)
         return affine[Affine._m_key] @ np.diag(1 / spacing)
 
     @staticmethod
-    def affine2comps(affine, spacing=None):
+    def affine2comps(
+        affine: NDArrayFloat, spacing: Optional[np.ndarray] = None
+    ) -> Tuple[NDArrayFloat, NDArrayFloat, NDArrayFloat]:
         if spacing is None:
             spacing = Affine.affine2spacing(affine)
         return (
